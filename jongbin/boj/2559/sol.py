2c5d2ec521ae972da445ea3ddc05c41dc0e22429
seohyun <imseohyun05@gmail.com>
2025-04-10 17:20:32 +0900

    [fail] : 런타임 에러로 해결 실패 [BOJ] : 2259

---
+++ jongbin/boj/2559/sol.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+result = []
+
+for i in range(n - m + 1):
+    num = sum(arr[i:i + m])
+    result.append(num)
+
+print(max(result))
